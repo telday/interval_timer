8238f290b987dffc44c7c6e0076a9ce8abe1fba1
telday <ejw393@gmail.com>
2020-04-03 14:01:28 -0400

    Added extra text for total time on timer

--- app/src/main/java/com/emomtimer/MainActivity.java
+++ app/src/main/java/com/emomtimer/MainActivity.java
@@ -7,14 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
-    private TextView timer;
+    private TextView loopTimer;
+    private TextView fullTimer;
+    private CountDownTimer countDownTimer = null;
 
     private void playSound(){
         this.mediaPlayer.start();
@@ -25,13 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button startButton = findViewById(R.id.startButton);
-        timer = findViewById(R.id.timerDisplay);
+        loopTimer = findViewById(R.id.loopTimer);
+        fullTimer = findViewById(R.id.fullTimer);
         mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.alarmsound);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playSound();
-                runTimer(2, 10);
+                runTimer(60, 3600);
             }
         });
     }
@@ -43,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
      * @param totalTime The total length of the timer in seconds
      */
     private void runTimer(final int loopLength, int totalTime){
-        CountDownTimer countDownTimer = new CountDownTimer(totalTime * 1000, 1000) {
+        if (countDownTimer != null){
+            countDownTimer.cancel();
+        }
+        countDownTimer = new CountDownTimer(totalTime * 1000, 1000) {
             private Long secondsPassed = new Long(0);
             @Override
             public void onTick(long millisUntilFinished) {
@@ -51,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
                 if (secondsPassed % loopLength == 0){
                     playSound();
                 }
-                timer.setText(secondsPassed.toString());
+                loopTimer.setText(new Long(secondsPassed % loopLength).toString());
+                fullTimer.setText(secondsPassed.toString());
             }
 
             @Override
