157d5a1016878fad36ae63e79f2718d9b5b0fcac
telday <ejw393@gmail.com>
2020-05-02 15:17:44 -0400

    Added functionality to settings menu

--- app/src/main/java/com/emomtimer/ui/SettingsFragment.java
+++ app/src/main/java/com/emomtimer/ui/SettingsFragment.java
@@ -1,25 +1,69 @@
 package com.emomtimer.ui;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.emomtimer.IntervalTimerApplication;
 import com.emomtimer.R;
+import com.emomtimer.timer.IntervalTimer;
 
 public class SettingsFragment extends Fragment {
-
+    EditText workTime;
+    EditText restTime;
+    EditText intervals;
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
+    }
+
+    private int getWorkTime(){
+        String text = workTime.getText().toString();
+        return Integer.valueOf(text);
+    }
+
+    private int getRestTime(){
+        String text = restTime.getText().toString();
+        return Integer.valueOf(text);
+    }
+
+    private int getIntervals(){
+        String text = intervals.getText().toString();
+        return Integer.valueOf(text);
+    }
+
+    private class SettingsTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            IntervalTimer timer = new IntervalTimer((long)getWorkTime(), (long)getRestTime(), getIntervals());
+            IntervalTimerApplication.timer = timer;
+        }
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        View view = this.getView();
+        workTime = view.findViewById(R.id.workTime);
+        restTime = view.findViewById(R.id.restTime);
+        intervals = view.findViewById(R.id.intervals);
+
+        workTime.addTextChangedListener(new SettingsTextWatcher());
+        restTime.addTextChangedListener(new SettingsTextWatcher());
+        intervals.addTextChangedListener(new SettingsTextWatcher());
         return inflater.inflate(R.layout.settings_fragment, container, false);
     }
 
